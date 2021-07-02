1496910da86b88a7a8e81e009f08393d554333a0
Fiso-Maker <youn110158@naver.com>
2021-07-02 17:10:02 +0900

    0.08

--- GSHS/Assets/Scripts/SceneLoader.cs
+++ GSHS/Assets/Scripts/SceneLoader.cs
@@ -7,6 +7,7 @@ using Fungus;
 public class SceneLoader : MonoBehaviour
 {
     public static SceneLoader instance;
+
     void Awake(){
         if(instance == null)
         {
@@ -21,39 +22,5 @@ public class SceneLoader : MonoBehaviour
     public void load(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
-    }
-
-    public void Start_Btn_Click()
-    {
-        try{
-            var Savemenu = GameObject.Find("SaveMenu");
-            Savemenu.transform.Find("Buttons").Find("MenuButton").gameObject.SetActive(true);
-            SaveMenu a = Savemenu.gameObject.GetComponent<SaveMenu>();
-            a.hasLoadedOnStart = true;
-
-            NarrativeLogMenu b = Savemenu.gameObject.GetComponent<NarrativeLogMenu>();
-            b.OnSaveReset();
-        }
-        catch (NullReferenceException){
-            Debug.Log("아직 버튼이 생성되지 않았습니다");
-        }
-        finally{
-            load("InGame_1");
-        }
-    }
-    public void Load_Btn_Click()
-    {
-        try{
-            var Savemenu = GameObject.Find("SaveMenu");
-            Savemenu.transform.Find("Buttons").Find("MenuButton").gameObject.SetActive(true);
-            SaveMenu a = Savemenu.gameObject.GetComponent<SaveMenu>();
-            a.hasLoadedOnStart = false;
-        }
-        catch (NullReferenceException){
-            Debug.Log("아직 버튼이 생성되지 않았습니다");
-        }
-        finally{
-            load("InGame_1");
-        }
     }
 }
--- GSHS/Assets/Scripts/UI_Manager.cs
+++ GSHS/Assets/Scripts/UI_Manager.cs
@@ -2,14 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using Fungus;
 
 public class UI_Manager : MonoBehaviour
 {
-    [SerializeField] CanvasGroup panel;
-    void Awake(){ 
+    public static UI_Manager instance;
+    
+    void Awake(){
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
-    public void Restart_BTN_Click()
+    public void Start_Btn_Click()
     {
+        try{
+            var Savemenu = GameObject.Find("SaveMenu");
+            Savemenu.transform.Find("Buttons").Find("MenuButton").gameObject.SetActive(true);
+            SaveMenu a = Savemenu.gameObject.GetComponent<SaveMenu>();
+            a.hasLoadedOnStart = true;
 
+            NarrativeLogMenu b = Savemenu.gameObject.GetComponent<NarrativeLogMenu>();
+            b.OnSaveReset();
+        }
+        catch (NullReferenceException){
+            Debug.Log("아직 버튼이 생성되지 않았습니다");
+        }
+        finally{
+            SceneLoader.instance.load("InGame_1");
+        }
+    }
+    public void Load_Btn_Click()
+    {
+        try{
+            var Savemenu = GameObject.Find("SaveMenu");
+            Savemenu.transform.Find("Buttons").Find("MenuButton").gameObject.SetActive(true);
+            SaveMenu a = Savemenu.gameObject.GetComponent<SaveMenu>();
+            a.hasLoadedOnStart = false;
+        }
+        catch (NullReferenceException){
+            Debug.Log("아직 버튼이 생성되지 않았습니다");
+        }
+        finally{
+            SceneLoader.instance.load("InGame_1");
+        }
+    }
+    public void Quit_Btn_Click()
+    {
+        Application.Quit();
     }
 }
